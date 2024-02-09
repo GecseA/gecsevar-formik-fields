b45a6968d3dd1bb41cc46cb5008cb3b91f011cdc
AstrA <gecsevar@gmail.com>
2024-02-09 17:18:39 +0100

    remove repack cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {TestComponent} from "./TestComponent";
+import {GVTextField} from "./GVTextField";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+type ButtonProps = {
+    children : JSX.Element
+}
+export function MyButton(props: ButtonProps) {
+    const {
+        children,
+        ...elementProps
+    } = props;
+    return <button type="button" {...elementProps}>{children}</button>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
